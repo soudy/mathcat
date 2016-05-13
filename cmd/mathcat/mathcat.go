fabb9090defa8299aa361d9ce72e32490e5c9d79
Steven Oud <soud@protonmail.com>
2016-05-13 16:33:57 +0200

    Keep track of REPL history

--- cmd/mathcat/mathcat.go
+++ cmd/mathcat/mathcat.go
@@ -5,13 +5,32 @@ import (
 	"fmt"
 	"github.com/soudy/mathcat"
 	"gopkg.in/readline.v1"
+	"os"
+	"runtime"
 )
 
 var precision = flag.Int("precision", 2, "decimal precision used in results")
 
+func getHomeDir() string {
+	if runtime.GOOS == "windows" {
+		home := os.Getenv("HOMEDRIVE") + os.Getenv("HOMEPATH")
+		if home == "" {
+			home = os.Getenv("USERPROFILE")
+		}
+
+		return home
+	}
+
+	return os.Getenv("HOME")
+}
+
 func repl() {
 	p := mathcat.New()
-	rl, err := readline.New("mathcat> ")
+	rl, err := readline.NewEx(&readline.Config{
+		Prompt:      "mathcat> ",
+		HistoryFile: getHomeDir() + "/.mathcat_history",
+	})
+
 	if err != nil {
 		panic(err)
 	}
