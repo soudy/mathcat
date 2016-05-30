70b2b101eeb898846da3e163457903b96a13db6c
Steven Oud <soud@protonmail.com>
2016-05-30 18:07:40 +0200

    Move all whitespace characters into slice

--- lexer.go
+++ lexer.go
@@ -23,11 +23,11 @@ type lexer struct {
 
 // https://en.wikipedia.org/wiki/Whitespace_character
 var whitespaceChars = []rune{
-	'\u0009', '\u000A', '\u000B', '\u000C', '\u000D', '\u0020', '\u0085',
-	'\u00A0', '\u1680', '\u2000', '\u2001', '\u2002', '\u2003', '\u2004',
-	'\u2005', '\u2006', '\u2007', '\u2008', '\u2009', '\u200A', '\u2028',
-	'\u2029', '\u202F', '\u205F', '\u3000', '\u180E', '\u200B', '\u200C',
-	'\u200D', '\u2060', '\uFEFF',
+	'\t', ' ', '\r', '\n', '\u0009', '\u000A', '\u000B', '\u000C', '\u000D',
+	'\u0020', '\u0085', '\u00A0', '\u1680', '\u2000', '\u2001', '\u2002',
+	'\u2003', '\u2004', '\u2005', '\u2006', '\u2007', '\u2008', '\u2009',
+	'\u200A', '\u2028', '\u2029', '\u202F', '\u205F', '\u3000', '\u180E',
+	'\u200B', '\u200C', '\u200D', '\u2060', '\uFEFF',
 }
 
 func isIdent(c rune) bool {
@@ -51,10 +51,6 @@ func isOctal(c rune) bool {
 }
 
 func isWhitespace(c rune) bool {
-	if c == '\t' || c == ' ' || c == '\r' {
-		return true
-	}
-
 	for _, v := range whitespaceChars {
 		if v == c {
 			return true
