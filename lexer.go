75426ce295793591ee1a304c1fc7b1d59e911d7c
Steven Oud <soud@protonmail.com>
2016-06-12 23:44:56 +0200

    Dont check ALL possible whitespace characters

--- lexer.go
+++ lexer.go
@@ -22,15 +22,6 @@ type lexer struct {
 	tokens []*Token // tokenized lexemes
 }
 
-// https://en.wikipedia.org/wiki/Whitespace_character
-var whitespaceChars = []rune{
-	'\t', ' ', '\r', '\n', '\u0009', '\u000A', '\u000B', '\u000C', '\u000D',
-	'\u0020', '\u0085', '\u00A0', '\u1680', '\u2000', '\u2001', '\u2002',
-	'\u2003', '\u2004', '\u2005', '\u2006', '\u2007', '\u2008', '\u2009',
-	'\u200A', '\u2028', '\u2029', '\u202F', '\u205F', '\u3000', '\u180E',
-	'\u200B', '\u200C', '\u200D', '\u2060', '\uFEFF',
-}
-
 func isIdent(c rune) bool {
 	return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_' || (c >= 0x80 && unicode.IsLetter(c))
 }
@@ -52,13 +43,7 @@ func isOctal(c rune) bool {
 }
 
 func isWhitespace(c rune) bool {
-	for _, v := range whitespaceChars {
-		if v == c {
-			return true
-		}
-	}
-
-	return false
+	return c == '\t' || c == ' ' || c == '\r' || c == '\n'
 }
 
 // Lex starts lexing an expression, converting an input string into a stream
